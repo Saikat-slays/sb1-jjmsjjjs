659430f9c59cd07b9e6da94b5b9c7f698c33e9c0
Saikat-slays <saikat@thynknext.in>
2025-06-05 22:23:01 +0530

    Fix blank screen issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -9,7 +8,6 @@ import StrategyCall from './pages/StrategyCall';
 import HowWeWork from './pages/HowWeWork';
 import TermsOfService from './pages/TermsOfService';
 import PrivacyPolicy from './pages/PrivacyPolicy';
-import { AuroraEffect } from './components/ui/aurora-effect';
 import { PageTransition } from './components/ui/motion';
 
 function ScrollToTop() {
@@ -27,18 +25,19 @@ function App() {
     <Router>
       <ScrollToTop />
       <div className="relative min-h-screen bg-charcoal">
-        <AuroraEffect />
         <div className="relative z-10">
           <Navbar />
-          <Routes>
-            <Route path="/" element={<PageTransition><Home /></PageTransition>} />
-            <Route path="/services" element={<PageTransition><Services /></PageTransition>} />
-            <Route path="/about" element={<PageTransition><About /></PageTransition>} />
-            <Route path="/strategy-call" element={<PageTransition><StrategyCall /></PageTransition>} />
-            <Route path="/how-we-work" element={<PageTransition><HowWeWork /></PageTransition>} />
-            <Route path="/terms" element={<PageTransition><TermsOfService /></PageTransition>} />
-            <Route path="/privacy" element={<PageTransition><PrivacyPolicy /></PageTransition>} />
-          </Routes>
+          <main>
+            <Routes>
+              <Route path="/" element={<PageTransition><Home /></PageTransition>} />
+              <Route path="/services" element={<PageTransition><Services /></PageTransition>} />
+              <Route path="/about" element={<PageTransition><About /></PageTransition>} />
+              <Route path="/strategy-call" element={<PageTransition><StrategyCall /></PageTransition>} />
+              <Route path="/how-we-work" element={<PageTransition><HowWeWork /></PageTransition>} />
+              <Route path="/terms" element={<PageTransition><TermsOfService /></PageTransition>} />
+              <Route path="/privacy" element={<PageTransition><PrivacyPolicy /></PageTransition>} />
+            </Routes>
+          </main>
           <Footer />
         </div>
       </div>
